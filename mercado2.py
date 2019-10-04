13232360b1305beaacb4bdbfca4e2bfc304b2e05
Israelfluz <israelfaria.gtr@gmail.com>
2019-10-04 08:47:30 -0300

    Pasta atualizada

--- mercado2.py
+++ mercado2.py
@@ -1,13 +1,20 @@
+# ====== importação da biblioteca =======
 import pandas as pd
 
+# ===== carregando a base de dados a ser trabalhanda ======
 dados = pd.read_csv('mercado2.csv', header = None)
+
+# ===== Convertendo o modelo Dataframe do pandas para uma lista ======
+# ===== O algoritmo apyori não recebe um Dataframe e sim uma lista =====
 transacoes = []
 for i in range(0, 7501):
     transacoes.append([str(dados.values[i,j]) for j in range(0, 20)])
 
+# ===== Importação do método responsável pelas regras ===== 
 from apyori import apriori
 regras = apriori(transacoes, min_support = 0.003, min_confidence = 0.2, min_lift = 2.0, min_length = 2)
 
+# ===== Variável que aplica o formato lista nas regras =====
 resultados = list(regras)
 resultados
 
